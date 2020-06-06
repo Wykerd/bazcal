72bb693d642d9ca6f45eb34d67ed1ad1239f994b
Daniel Wykerd <danielwykerd@gmail.com>
2020-06-07 00:50:43 +0200

    Massive optimizations

--- api/src/routes/stats.ts
+++ api/src/routes/stats.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import APICache from '../models/cache';
 import createError from "http-errors";
-import { volitility_calc } from "../utils";
+import fs from 'fs';
+import path from 'path';
 
 export const StatsCalc : express.RequestHandler = async (_req, res, next) => {
     try {
-        res.status(200).json(volitility_calc(await APICache.find()));
+        const f_path = path.resolve(process.env.DATA_DIR ?? './', 'stats.json');
+        res.status(200).json(await fs.promises.readFile(f_path));
     } catch (error) {
         console.error(error);
         next(createError(500, 'Internal API error.')); // just catch all, I'm to lazy to check for each type...
--- scheduler/src/jobs/index.js
+++ scheduler/src/jobs/index.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch'
 import APICache from '../models/cache'
+import fs from 'fs'
+import path from 'path'
 
 /**
  * Jobs
@@ -17,9 +19,24 @@ export function CachingJobs (agenda) {
                 timestamp: new Date()
             });
 
-            const saved = await cache.save();
+            const doc = await cache.save();
 
-            await agenda.schedule('in a week', 'cache:purge', { cache_id: saved._id });
+            const regen = {};
+
+            for (const product in (doc.response?.products ?? {})) {
+                const prod = doc.response?.products[product];
+                if (!prod) continue;
+                regen[product] = {};
+                regen[product].buy = prod.quick_status?.buyMovingWeek ?? 0;
+                regen[product].sell = prod.buy_summary?.[0]?.pricePerUnit ?? 0;
+                regen[product].volume = prod.sell_summary?.[0]?.pricePerUnit ?? 0;
+                regen[product].svolume = prod.quick_status?.sellMovingWeek ?? 0;
+                regen[product].count = 1;
+            }
+
+            await agenda.now('cache:regenerate_stats', regen);
+
+            await agenda.schedule('in a week', 'cache:purge', { cache_id: doc._id });
         } catch (err) {
             console.error('JOB FAILED', err);
             job.fail(err);
@@ -28,8 +45,63 @@ export function CachingJobs (agenda) {
 
     agenda.define('cache:purge', async job => {
         try {
-            await APICache.findByIdAndDelete(job.attrs.data._id);
+            console.log('Purging old cached response...');
+            const doc = await APICache.findByIdAndDelete(job.attrs.data._id);
+
+            const regen = {};
+
+            for (const product in (doc.response?.products ?? {})) {
+                const prod = doc.response?.products[product];
+                if (!prod) continue;
+                regen[product] = {};
+                regen[product].buy = -prod.quick_status?.buyMovingWeek ?? 0;
+                regen[product].sell = -prod.buy_summary?.[0]?.pricePerUnit ?? 0;
+                regen[product].volume = -prod.sell_summary?.[0]?.pricePerUnit ?? 0;
+                regen[product].svolume = -prod.quick_status?.sellMovingWeek ?? 0;
+                regen[product].count = -1;
+            }
+
+            await agenda.now('cache:regenerate_stats', regen);
+
             await job.remove();
+        } catch (error) {
+            console.error('JOB FAILED', err);
+            job.fail(err);
+        }
+    });
+
+    agenda.define('cache:regenerate_stats', { concurrency: 1 }, async job => {
+        try {
+            console.log('Regenerating stats...');
+            const f_path = path.resolve(process.env.DATA_DIR ?? './', 'stats.json');
+            let file_contents;
+            try {
+                file_contents = await fs.promises.readFile(f_path, 'utf-8');  
+            } catch (error) {
+                file_contents = "{}";
+            } 
+            const stats = JSON.parse(file_contents);
+            Object.keys(job.attrs.data).forEach(key => {
+                const patch = job.attrs.data[key];
+                if (patch) {
+                    if (!stats[key]) stats[key] = {
+                        buy: [0,0],
+                        sell: [0,0],
+                        volume: 0,
+                        svolume: 0,
+                        count: 0
+                    };
+
+                    stats[key].buy[0] += patch.buy;
+                    stats[key].sell[0] += patch.sell;
+                    stats[key].volume += patch.volume;
+                    stats[key].svolume += patch.svolume;
+                    stats[key].buy[1] += (patch.buy * patch.buy) / 100;
+                    stats[key].sell[1] += (patch.sell * patch.sell) / 100;
+                    stats[key].count += patch.count;
+                }
+            });
+            await fs.promises.writeFile(f_path, JSON.stringify(stats));
         } catch (error) {
             console.error('JOB FAILED', err);
             job.fail(err);
