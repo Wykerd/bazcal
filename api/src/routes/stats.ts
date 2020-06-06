7c6d22789327d4a79cd5d0dcf13b9e1a3a3f8e72
Daniel Wykerd <danielwykerd@gmail.com>
2020-06-07 00:56:58 +0200

    Fix stats

--- api/src/routes/stats.ts
+++ api/src/routes/stats.ts
@@ -7,7 +7,7 @@ import path from 'path';
 export const StatsCalc : express.RequestHandler = async (_req, res, next) => {
     try {
         const f_path = path.resolve(process.env.DATA_DIR ?? './', 'stats.json');
-        res.status(200).json(await fs.promises.readFile(f_path));
+        res.status(200).json(JSON.parse(await fs.promises.readFile(f_path, 'utf-8')));
     } catch (error) {
         console.error(error);
         next(createError(500, 'Internal API error.')); // just catch all, I'm to lazy to check for each type...
