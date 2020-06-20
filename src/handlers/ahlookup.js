4894bdfecc22a29e1912c5bc85ddc1a5ba241282
Daniel Wykerd <danielwykerd@gmail.com>
2020-06-21 01:36:38 +0200

    ahlookup fix null

--- src/handlers/ahlookup.js
+++ src/handlers/ahlookup.js
@@ -6,7 +6,7 @@ const AuctionLookupHandler = async (message, args) => {
 
     const item_data = await ( await fetch(url + item_name)).json()
 
-    if (!item_data) throw new Error('Item with name **' + args[0] + '** not found')
+    if (!item_data?.average_price) throw new Error('Item with name **' + args[0] + '** not found')
 
     message.channel.send(`<@${message.author.id}>\nAverage Price: **${item_data.average_price}**\nMedian Price: **${item_data.median_price}**\nStandard Deviation: **${item_data.standard_deviation}**\n\nMax Price: **${item_data.max_price}**\nMinimum Price: **${item_data.min_price}**\n\nItems sold in the last 24h: **${item_data.sold}**`)
 }
